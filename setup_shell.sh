42392c776759f26ef48a7462b1f06ec0b846ef45
5Paisa <vishwesh5paisa@gmail.com>
2025-06-09 10:57:49 +0530

    Update setup_shell.sh

--- setup_shell.sh
+++ setup_shell.sh
@@ -2,6 +2,6 @@ pip install py5paisa
 pip3 install py5paisa
 pip install pyotp
 pip3 install pyotp
-npm install -g ts-node   
-npm install -g mcp-framework
-npm install
+sudo npm install -g ts-node   
+sudo npm install -g mcp-framework
+sudo npm install
